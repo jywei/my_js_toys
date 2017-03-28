116e7864fc05611b9b647636711a038d4d251606
Roy Wei <roywjy@gmail.com>
2017-03-28 15:41:06 +0800

    Add ESLint

--- yarn_pg/src/index.js
+++ yarn_pg/src/index.js
@@ -8,3 +8,9 @@ console.log('Hello world')
 
 const str = 'ES6'
 console.log(`Hello ${str}`)
+
+// const Dog = require('./dog')
+import Dog from './dog'
+const toby = new Dog('Toby')
+
+console.log(toby.bark())
