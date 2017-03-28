d85aa6b8c911bce1d8e1ed45697af1a7c4b86ac9
Roy Wei <roywjy@gmail.com>
2017-03-28 15:31:36 +0800

    Add babel-cli

---
+++ yarn_pg/src/index.js
@@ -0,0 +1,10 @@
+console.log('Hello world')
+
+// const Color = require('color')
+
+// const redHexa = Color({r: 255, g: 0, b: 0}).hex()
+
+// console.log(redHexa)
+
+const str = 'ES6'
+console.log(`Hello ${str}`)
