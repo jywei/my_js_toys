63503237a29d3a42d1d6e49115ccd3a7a7879796
Roy Wei <roywjy@gmail.com>
2017-04-04 23:29:56 +0800

    Absolute imports should come before relative imports

--- yarn_pg/src/client/index.jsx
+++ yarn_pg/src/client/index.jsx
@@ -6,6 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
@@ -24,9 +25,11 @@ const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
 const wrapApp = (AppComponent, reduxStore) =>
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>
 
 ReactDOM.render(wrapApp(App, store), rootEl)
