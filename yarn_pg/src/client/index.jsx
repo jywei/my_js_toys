60d94a9aa7c8c7519d470879ba62aef16fddfd51
Roy Wei <roywjy@gmail.com>
2017-04-05 23:34:43 +0800

    Add jquery and tether

--- yarn_pg/src/client/index.jsx
+++ yarn_pg/src/client/index.jsx
@@ -10,12 +10,18 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import $ from 'jquery'
+import Tether from 'tether'
 
 import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 import setUpSocket from './socket'
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
